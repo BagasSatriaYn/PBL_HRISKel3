9d29b4f2f6dda92046f0fa522e5764bb638efb41
Fallujahrama <fallujahrc@gmail.com>
2025-11-21 20:23:58 +0700

    Create: folder Client isinya flutter

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\LetterFormatController;
-use App\Http\Controllers\Api\LetterController;
-
-Route::apiResource('letter-formats', LetterFormatController::class);
-Route::apiResource('letters', LetterController::class);
